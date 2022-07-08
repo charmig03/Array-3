88788d18aaef6cb5ba6ea3fc2e734b4d9f60b6e4
charmii03 <charmigangani46@gmail.com>
2022-07-08 22:49:48 +0530

    js completed

---
+++ squareUp.js
@@ -0,0 +1,16 @@
+function squareUp(n){
+    let subArray = [];
+  let j = n;
+  while (j > 0) {
+    subArray.push(0);
+    j--;
+  }
+
+  let output = [];
+
+  for (let i = n - 1; i >= 0; i--) {
+    subArray.splice(i, 1, n - i);
+    output = output.concat(subArray);
+  }
+  return output;
+}
